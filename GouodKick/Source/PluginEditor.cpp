81a91e92c30a10cb8c488e40d153147b09e1bcd8
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2023-11-17 22:54:37 +0100

    Add files via upload

--- GouodKick/Source/PluginEditor.cpp
+++ GouodKick/Source/PluginEditor.cpp
@@ -9,38 +9,44 @@
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
+
 //==============================================================================
 GouodKickAudioProcessorEditor::GouodKickAudioProcessorEditor (GouodKickAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (500, 300);
+    setSize (472, 673);
+    setLookAndFeel(&gouodKickLookAndFeel);
 
     highFrequencySlider.setSliderStyle(juce::Slider::SliderStyle::LinearBarVertical);
     highFrequencySlider.setRange(0.0, 2.0, 0.1);
-    highFrequencySlider.setTextValueSuffix(" High Part");
+    //highFrequencySlider.setTextValueSuffix(" High Part");
+    highFrequencySlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     highFrequencySlider.setValue(1.0f);
     highFrequencySlider.addListener(this);
     addAndMakeVisible(&highFrequencySlider);
 
     lowFrequencySlider.setSliderStyle(juce::Slider::SliderStyle::LinearBarVertical);
     lowFrequencySlider.setRange(0.0, 3.0, 0.1);
-    lowFrequencySlider.setTextValueSuffix(" Low Part");
+    //lowFrequencySlider.setTextValueSuffix(" Low Part");
+    lowFrequencySlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     lowFrequencySlider.setValue(1.0f);
     lowFrequencySlider.addListener(this);
     addAndMakeVisible(&lowFrequencySlider);
 
     lowGainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearBarVertical);
     lowGainSlider.setRange(0.0, 100.0, 0.1);
-    lowGainSlider.setTextValueSuffix(" Low Gain");
+    //lowGainSlider.setTextValueSuffix(" Low Gain");
+    lowGainSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     lowGainSlider.setValue(1.0f);
     lowGainSlider.addListener(this);
     addAndMakeVisible(&lowGainSlider);
 
     highGainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearBarVertical);
     highGainSlider.setRange(0.0, 100.0, 0.1);
-    highGainSlider.setTextValueSuffix(" High Gain");
+    //highGainSlider.setTextValueSuffix(" High Gain");
+    highGainSlider.setTextBoxStyle(Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
     highGainSlider.setValue(1.0f);
     highGainSlider.addListener(this);
     addAndMakeVisible(&highGainSlider);
@@ -56,10 +62,17 @@ void GouodKickAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+    Image background = ImageCache::getFromMemory (BinaryData::KICKTATOR_V2_BG_png, BinaryData::KICKTATOR_V2_BG_pngSize);
+    g.drawImageAt (background, 0, 0);
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
     g.drawFittedText ("Made by Gouod Labs", getLocalBounds(), juce::Justification::bottomRight, 1);
+
+    g.fillRect(52, 235 - (lowGainSlider.getValue()/lowGainSlider.getMaximum()) * 122, 41, (lowGainSlider.getValue()/lowGainSlider.getMaximum()) * 122 );
+    g.fillRect(159, 235 - (lowFrequencySlider.getValue()/lowFrequencySlider.getMaximum()) * 122, 41, (lowFrequencySlider.getValue()/lowFrequencySlider.getMaximum()) * 122 );
+    g.fillRect(275, 235 - (highGainSlider.getValue()/highGainSlider.getMaximum()) * 122, 41, (highGainSlider.getValue()/highGainSlider.getMaximum()) * 122 );
+    g.fillRect(382, 235 - (highFrequencySlider.getValue()/highFrequencySlider.getMaximum()) * 122, 41, (highFrequencySlider.getValue()/highFrequencySlider.getMaximum()) * 122 );
 }
 
 void GouodKickAudioProcessorEditor::resized()
@@ -68,10 +81,12 @@ void GouodKickAudioProcessorEditor::resized()
     const int dialWidth = getWidth() / 2 - border;
     const int dialHight = getHeight() - border - 100;
 
-    highFrequencySlider.setBounds(border, border, 100, 260);
-    lowFrequencySlider.setBounds(2 * border + 100, border, 100, 260);
-    lowGainSlider.setBounds(3 * border + 2 * 100, border, 100, 260);
-    highGainSlider.setBounds(4 * border + 3 * 100, border, 100, 260);
+    
+    lowGainSlider.setBounds(33, 338, 48, 310);
+    lowFrequencySlider.setBounds(114, 338, 48, 310);
+
+    highGainSlider.setBounds(312, 338, 48, 310);
+    highFrequencySlider.setBounds(312 + 114 - 33, 338, 48, 310);
 }
 
 void GouodKickAudioProcessorEditor::sliderValueChanged(juce::Slider* slider) {
@@ -81,4 +96,5 @@ void GouodKickAudioProcessorEditor::sliderValueChanged(juce::Slider* slider) {
         lowGainSlider.getValue(),
         highGainSlider.getValue()
     );
+    repaint();
 }
--- GouodKick/Source/PluginEditor.h
+++ GouodKick/Source/PluginEditor.h
@@ -15,7 +15,35 @@
 /**
 */
 
+
+
 using namespace juce;
+
+
+class GouodKickLookAndFeel : public juce::LookAndFeel_V4 {
+public:
+    GouodKickLookAndFeel()
+    {
+        setColour (juce::Slider::thumbColourId, juce::Colours::red);
+    }
+    void drawLinearSlider (Graphics &g, int x, int y, int width, int height, float sliderPos, float minSliderPos, float maxSliderPos, Slider::SliderStyle, Slider &) override {
+        Image sliderKnob = ImageCache::getFromMemory (BinaryData::FADER_SHADOW_png, BinaryData::FADER_SHADOW_pngSize);
+        float mid = height/2;
+        //200 - 50 / 2 + 50 = 125
+        //100 - 20 / 2 + 20 = 60 
+
+        // 60 - 20 / 100 - 20
+        if(mid < (sliderPos - minSliderPos)/(maxSliderPos - minSliderPos) * height) {
+            g.setColour(juce::Colours::grey);
+            g.fillRect(x + 20 * 1.0, y + (height/2) * 1.0, 10 * 1.0, (sliderPos - minSliderPos)/(maxSliderPos - minSliderPos) * height * 0.5f);
+        }
+        g.drawImageAt (sliderKnob.rescaled(sliderKnob.getWidth()/4, sliderKnob.getHeight()/4, Graphics::mediumResamplingQuality), x, (y + sliderPos - 40) * 0.95);
+    }
+
+private:
+
+};
+
 class GouodKickAudioProcessorEditor  : public juce::AudioProcessorEditor,public Slider::Listener
 {
 public:
@@ -38,6 +66,7 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     GouodKickAudioProcessor& audioProcessor;
+    GouodKickLookAndFeel gouodKickLookAndFeel;
 
     void sliderValueChanged(juce::Slider* slider) override;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (GouodKickAudioProcessorEditor)
